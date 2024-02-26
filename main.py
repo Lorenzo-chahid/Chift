457c7e32828855937e7c109aaf817a4dd8b29828
lorenzo chahid <clkwebdeveloper@gmail.com>
2024-02-26 11:18:30 +0100

    fat man

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import json
+from databases import Database
+import sqlalchemy
+
+DATABASE_URL = "postgres://admin:LYbMkR6K8JnwIOSkaaRf0YFQuUD0dOOn@dpg-cndss1mct0pc73824l20-a.frankfurt-postgres.render.com/chift_nc3y"
+
+database = Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+
+contacts = sqlalchemy.Table(
+    "contacts",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("name", sqlalchemy.String),
+    sqlalchemy.Column("email", sqlalchemy.String),
+    sqlalchemy.Column("phone", sqlalchemy.String),
+    sqlalchemy.Column("is_company", sqlalchemy.Boolean),
+)
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
 
 @app.get("/")
@@ -17,29 +43,21 @@ def read_root():
 
 @app.get("/contacts")
 async def read_contacts(skip: int = 0, limit: int = 12):
-    with open("contacts.json", "r") as json_file:
-        contacts = json.load(json_file)
-
-    # Appliquer la pagination
-    paginated_contacts = contacts[skip : skip + limit]
-    return paginated_contacts
+    query = contacts.select().offset(skip).limit(limit)
+    return await database.fetch_all(query)
 
 
 @app.get("/contacts/count")
 async def get_contacts_count():
-    print("YOUPI")
-    with open("contacts.json", "r") as json_file:
-        contacts = json.load(json_file)
-    return {"count": len(contacts)}
+    query = sqlalchemy.sql.select([sqlalchemy.func.count()]).select_from(contacts)
+    total_count = await database.fetch_val(query)
+    return {"count": total_count}
 
 
 @app.get("/contact/{contact_id}", response_class=HTMLResponse)
-def read_contact(request: Request, contact_id: int):
-    with open("contacts.json", "r") as json_file:
-        contacts = json.load(json_file)
-    contact = next(
-        (c for c in contacts if str(c.get("id", "")) == str(contact_id)), None
-    )
+async def read_contact(request: Request, contact_id: str):
+    query = contacts.select().where(contacts.c.id == contact_id)
+    contact = await database.fetch_one(query)
     if contact:
         return templates.TemplateResponse(
             "contact_details.html", {"request": request, "contact": contact}
