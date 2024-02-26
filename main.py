e4a4f7312524711574cc829fa1ff96af9ab250c4
Lorenzo <55135469+Lorenzo-chahid@users.noreply.github.com>
2024-02-26 12:33:30 +0100

    Staging (#1)
    update main with staging 
    
    ---------
    
    Co-authored-by: lorenzo chahid <clkwebdeveloper@gmail.com>

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
-import json
+from fastapi.templating import Jinja2Templates
+from databases import Database
+from dotenv import load_dotenv
+import sqlalchemy
+import os
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+database = Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+
+contacts = sqlalchemy.Table(
+    "contacts",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("name", sqlalchemy.String),
+    sqlalchemy.Column("email", sqlalchemy.String),
+    sqlalchemy.Column("phone", sqlalchemy.String),
+    sqlalchemy.Column("is_company", sqlalchemy.Boolean),
+)
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
 
 @app.get("/")
@@ -14,7 +45,25 @@ def read_root():
 
 
 @app.get("/contacts")
-async def read_contacts():
-    with open("contacts.json", "r") as json_file:
-        contacts = json.load(json_file)
-    return contacts
+async def read_contacts(skip: int = 0, limit: int = 12):
+    query = contacts.select().offset(skip).limit(limit)
+    return await database.fetch_all(query)
+
+
+@app.get("/contacts/count")
+async def get_contacts_count():
+    query = sqlalchemy.sql.select([sqlalchemy.func.count()]).select_from(contacts)
+    total_count = await database.fetch_val(query)
+    return {"count": total_count}
+
+
+@app.get("/contact/{contact_id}", response_class=HTMLResponse)
+async def read_contact(request: Request, contact_id: str):
+    query = contacts.select().where(contacts.c.id == contact_id)
+    contact = await database.fetch_one(query)
+    if contact:
+        return templates.TemplateResponse(
+            "contact_details.html", {"request": request, "contact": contact}
+        )
+    else:
+        raise HTTPException(status_code=404, detail="Contact not found")
